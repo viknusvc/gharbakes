1aa8f6efe9b673d4c493c0a6229085c85c5fc6b8
viknusvc <114333410+viknusvc@users.noreply.github.com>
2025-02-04 12:43:57 +0530

    Update scripts.js revert the chagne

--- js/scripts.js
+++ js/scripts.js
@@ -117,8 +117,8 @@ function viewCart() {
                         <button onclick="updateCartQuantity(${index}, -${stepValue})">-</button>
                         <input type="text" value="${item.quantity}" readonly id="cart-qty-${index}">
                         <button onclick="updateCartQuantity(${index}, ${stepValue})">+</button>
-                        <button onclick="removeCartItem(${index})" class="remove">Remove</button>
                     </div>
+                     <button onclick="removeCartItem(${index})" class="remove">Remove</button>
                 </div>
             `;
             cartContainer.appendChild(cartItem);
